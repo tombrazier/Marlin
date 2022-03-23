ff5e91a39fa883b6007064de9655c61274f0d27e
Tom Brazier <tom_github@firstsolo.net>
2022-03-23 18:12:45 +0000

    Stepper current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2817,7 +2817,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT       500
+    #define E0_CURRENT       600
     #define E0_MICROSTEPS    (TERN(GEARED_EXTRUDER, 2, 16))
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
