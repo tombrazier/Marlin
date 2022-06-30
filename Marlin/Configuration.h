2bcd8627b89b5b07ca1dbece144b075740201a76
Tom Brazier <tom_github@firstsolo.net>
2022-06-30 07:51:13 +0100

    max E stepper rate - VDE is safe with 12

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1038,7 +1038,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 100, 100, 5, 10 }
+#define DEFAULT_MAX_FEEDRATE          { 100, 100, 5, 12 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
