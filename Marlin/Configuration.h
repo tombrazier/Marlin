313bdff8ba6cb4d3059a9021439def27e24c0ae6
Tom Brazier <tom_github@firstsolo.net>
2023-03-04 17:28:47 +0000

    Removed skew correction for testing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2230,7 +2230,7 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-#define SKEW_CORRECTION
+//#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
