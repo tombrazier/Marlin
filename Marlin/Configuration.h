b29cdf046656fb626edf03fdedb4ace3639abb39
cdsmith <csmith@lulzbot.com>
2023-10-12 10:41:13 -0500

    Raise move between probe points higher to clear mag bed grab handles.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2225,7 +2225,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE    5 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
+#define Z_CLEARANCE_BETWEEN_PROBES  8 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
 #define Z_PROBE_TOLERANCE           6 // Farthest distance above the trigger-point without failed probe
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
