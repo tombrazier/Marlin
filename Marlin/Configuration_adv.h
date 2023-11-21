0f5d00243b6c85cd34e2ec76201ce30fbf415314
bkahl <bkahl@lulzbot.com>
2023-11-21 08:07:48 -0600

    🧑‍💻update screen timeout to 15 mins

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1696,7 +1696,7 @@
   #define SOUND_ON_DEFAULT    // Buzzer/speaker default enabled state
 
   // The timeout to return to the status screen from sub-menus
-  #define LCD_TIMEOUT_TO_STATUS 15000   // (ms)
+  #define LCD_TIMEOUT_TO_STATUS (15*60*1000)   // (ms) 15 minutes for screen timeout
 
   // Scroll a longer status message into view
   #define STATUS_MESSAGE_SCROLLING
