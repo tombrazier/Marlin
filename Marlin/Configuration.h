6f980d1d144da5c688d8f4a0e02341592fdf79af
Robert Collins <rcollins@lulzbot.com>
2023-12-12 10:34:55 -0600

    config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2544,10 +2544,10 @@
     #define X_BED_SIZE 285
     #define Y_BED_SIZE 285
     // Travel limits (mm) after homing, corresponding to endstop positions.
-    #define LULZBOT_X_MIN_POS -1
+    #define LULZBOT_X_MIN_POS -6
     #define LULZBOT_Y_MIN_POS -13.5
     #define LULZBOT_X_MAX_POS 313
-    #define LULZBOT_Y_MAX_POS 318
+    #define LULZBOT_Y_MAX_POS 330
     #define LULZBOT_Z_MIN_POS -9
     #define LULZBOT_Z_MAX_POS 299
   #endif
