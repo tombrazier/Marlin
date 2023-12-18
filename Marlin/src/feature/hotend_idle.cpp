e7e59247449ce01bc4f10ae15c29833476ba85c6
bmkahl <bkahl@lulzbot.com>
2023-12-18 14:39:31 -0600

    🔨 Fixing the Bed timeout to function and shut off the bed heater

--- Marlin/src/feature/hotend_idle.cpp
+++ Marlin/src/feature/hotend_idle.cpp
@@ -48,15 +48,17 @@ void HotendIdleProtection::check_hotends(const millis_t &ms) {
   bool do_prot = false;
   if (!busy && cfg.timeout != 0) {
     HOTEND_LOOP() {
-      if (thermalManager.degHotend(e) >= cfg.trigger) {
+      if ((thermalManager.degHotend(e) >= cfg.trigger) || (thermalManager.degBed() >= cfg.bed_trigger)) {
         do_prot = true; break;
       }
     }
   }
-  if (!do_prot)
+  if (!do_prot){
     next_protect_ms = 0;                          // No hotends are hot so cancel timeout
-  else if (!next_protect_ms){                      // Timeout is possible?
-    next_protect_ms = ms + 1000UL * cfg.timeout;  // Start timeout if not already set
+    next_bed_protect_ms = 0;                          // Bed is not hot so cancel timeout
+  }
+  else if (!next_protect_ms || !next_bed_protect_ms) {                     // Timeout is possible?
+    next_protect_ms = ms + 1000UL * cfg.timeout;  // Start timeout if not already set                   // Timeout is possible?
     next_bed_protect_ms = ms + 1000UL * cfg.bed_timeout;  // Start timeout of bed if not already set
   }
 }
@@ -94,7 +96,7 @@ void HotendIdleProtection::timed_out() {
   }
 }
 void HotendIdleProtection::bed_timed_out() {
-  next_protect_ms = 0;
+  next_bed_protect_ms = 0;
   SERIAL_ECHOLNPGM("Bed Idle Timeout");
   LCD_MESSAGE(MSG_BED_IDLE_TIMEOUT);
   #if HAS_HEATED_BED
