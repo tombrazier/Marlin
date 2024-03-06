37dda2751d4960f1fe1742638352cc2a68b99451
bmkahl <bkahl@lulzbot.com>
2024-03-06 11:03:05 -0600

    🔨 Fix what fan E0_AUTO_FAN_PIN is in

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -692,7 +692,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-//#define E0_AUTO_FAN_PIN 96
+//#define E0_AUTO_FAN_PIN 99
 //#define E1_AUTO_FAN_PIN 101
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
