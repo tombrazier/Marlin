bc17068c861fdadc4575ef7ff87c658cdb8b66af
bmkahl <bkahl@lulzbot.com>
2024-03-22 16:22:37 -0500

    🔧Reverting to Using double touch probes for non-BLTouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2269,7 +2269,9 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-//#define MULTIPLE_PROBING 2
+#if DISABLED(LULZBOT_BLTouch)
+  #define MULTIPLE_PROBING 2
+#endif
 //#define EXTRA_PROBING    1
 
 /**
