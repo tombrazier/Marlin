0810ff0dd99c2a9b12621c21fdb7e76fbb4a45dd
bmkahl <bkahl@lulzbot.com>
2024-05-01 15:52:38 -0500

    🔨Decrease watch temp on Long Bed to prevent excessive Thermal runaways

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -342,7 +342,11 @@
    * As described above, except for the bed (M140/M190/M303).
    */
   #define WATCH_BED_TEMP_PERIOD                60 // (seconds)
-  #define WATCH_BED_TEMP_INCREASE               2 // (°C)
+  #if ENABLED(LULZBOT_LONG_BED)
+    #define WATCH_BED_TEMP_INCREASE               1 // (°C)
+  #else
+    #define WATCH_BED_TEMP_INCREASE               2 // (°C)
+  #endif
 #endif
 
 /**
